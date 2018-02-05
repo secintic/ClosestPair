7fae9aacf88f64045fbb8e6e99d4d6650b0dd645
Cihan Secinti <cihan.secinti@accenture.com>
2018-02-05 18:52:36 +0300

    median closest points are calculated and algorithm is working fine now

--- src/main/java/ClosestPair.java
+++ src/main/java/ClosestPair.java
@@ -1,18 +1,31 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ClosestPair {
     private List<Point> points;
+    private int numberOfDimensions;
+    private Result result;
 
     ClosestPair(ArrayList<Point> points) {
         this.points = points;
+        this.numberOfDimensions = points.get(0).getAxes().length;
+        this.result = new Result();
     }
 
     public Result getMinDistPair() {
-        points.sort(Comparator.comparing(p -> p.getAxes()[0]));
-        return getMinDistPair(points);
+        AtomicInteger dimensionCounter = new AtomicInteger(-1);
+        while (dimensionCounter.incrementAndGet() < numberOfDimensions) {
+            points.sort(Comparator.comparing(p -> p.getAxes()[dimensionCounter.get()]));
+            if (getMinDistPair(points).getDelta() < result.getDelta())
+                result = getMinDistPair(points);
+        }
+        return result;
     }
 
     private Result getMinDistPair(List<Point> points) {
+        Result tempResult;
         int length = points.size();
         switch (length) {
             case 1:
@@ -21,14 +34,27 @@ public class ClosestPair {
                 return new Result(points.get(0), points.get(1)) {
                 };
             default:
-                List<Point> lower = points.subList(0, length / 2);
-                List<Point> upper = points.subList(length / 2, length);
+                int median = length / 2;
+                List<Point> lower = points.subList(0, median);
+                List<Point> upper = points.subList(median + 1, length);
                 Result resultLeft = getMinDistPair(lower);
                 Result resultRight = getMinDistPair(upper);
-                if (resultLeft.getDelta() < resultRight.getDelta()) {
-                    return resultLeft;
-                } else
-                    return resultRight;
+                if (resultLeft.getDelta() < resultRight.getDelta())
+                    tempResult = resultLeft;
+                else
+                    tempResult = resultRight;
+                List<Point> medianCloserPointList = new ArrayList<>();
+                for (Point point : points) {
+                    if (point.distanceTo(points.get(median)) < tempResult.getDelta())
+                        medianCloserPointList.add(point);
+                }
+                for (int i = 0; i < medianCloserPointList.size(); i++) {
+                    for (int j = i + 1; j < medianCloserPointList.size(); j++) {
+                        if (medianCloserPointList.get(i).distanceTo(medianCloserPointList.get(j)) < tempResult.getDelta())
+                            tempResult = new Result(medianCloserPointList.get(i), medianCloserPointList.get(j));
+                    }
+                }
+                return tempResult;
         }
     }
 }
--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -3,7 +3,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Result {
 
@@ -12,6 +11,10 @@ public class Result {
 
     Result(Point p1, Point p2) {
         this(p1, p2, p1.distanceTo(p2));
+    }
+
+    Result() {
+        this.delta = Double.MAX_VALUE;
     }
 
     @Override
